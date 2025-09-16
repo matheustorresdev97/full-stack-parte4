1cd983e2a07db5b309c2218daba346e7f1832e6e
matheustorresdev97 <matheustorresdev@gmail.com>
2025-09-16 19:57:02 -0300

    finalizando routes manager

---
+++ refund/src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export function Loading() {
+  return (
+    <div className="w-full min-h-screen h-auto flex justify-center items-center">
+      <span className="text-gray-200 font-semibold text-sm">Carregando...</span>
+    </div>
+  );
+}
--- refund/src/routes/index.tsx
+++ refund/src/routes/index.tsx
@@ -3,10 +3,35 @@ import { AuthRoutes } from "./auth.routes";
 import { EmployeeRoutes } from "./employee.routes";
 import { ManagerRoutes } from "./manager.routes";
 
+import { Loading } from "../components/Loading";
+
+const isLoading = false;
+
+const session = {
+  user: {
+    role: "manager",
+  },
+};
+
 export function Routes() {
+  function Route() {
+    switch (session?.user.role) {
+      case "employee":
+        return <EmployeeRoutes />;
+      case "manager":
+        return <ManagerRoutes />;
+      default:
+        return <AuthRoutes />;
+    }
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <BrowserRouter>
-      <ManagerRoutes />
+      <Route />
     </BrowserRouter>
   );
 }
