07705058484987013bddc4340aeccab2cd0a3e0e
matheustorresdev97 <matheustorresdev@gmail.com>
2025-09-16 18:40:21 -0300

    header e utils

--- adivinhe/src/App.tsx
+++ adivinhe/src/App.tsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
+import styles from "./app.module.css";
+import { Header } from "./components/Header";
+import type { GuessProps } from "./components/LettersUsed";
+import { WORDS, type Challenge } from "./utils/words";
+
 export default function App() {
+  const [score, setScore] = useState(0);
+  const [letter, setLetter] = useState("");
+  const [guesses, setGuesses] = useState<GuessProps[]>([]);
+  const [challenge, setChallenge] = useState<Challenge | null>(null);
+
+  const ATTEMPT_LIMIT = 10;
+
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length);
+    const randomWord = WORDS[index];
+
+    setChallenge(randomWord);
+
+    setScore(0);
+    setLetter("");
+    setGuesses([]);
+  }
+
+  function handleRestartGame() {
+    const isConfirmed = window.confirm(
+      "Você tem certeza que deseja reiniciar?"
+    );
+
+    if (isConfirmed) {
+      startGame();
+    }
+  }
+
   return (
-    <div>
-      <h1>Olá Mundo!</h1>
+    <div className={styles.container}>
+      <main>
+        <Header
+          max={ATTEMPT_LIMIT}
+          current={guesses.length}
+          onRestart={handleRestartGame}
+        />
+      </main>
     </div>
-  )
+  );
 }
